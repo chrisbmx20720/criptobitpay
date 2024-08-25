cb63708bf1a339856fae64de98f2af7fce61e647
chrisbmx20720 <christian.al.ac20@gmail.com>
2024-08-24 22:51:20 -0600

    indices mostrandose

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { InicioComponent } from './components/inicio/inicio.component';
-import { MessagesComponent } from './components/messages/messages.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { UserComponent } from './components/user/user.component';
-
-import{ DashboardComponent } from './components/dashboard/dashboard.component'
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent, children: [
-    { path: '', redirectTo: 'inicio', pathMatch: 'full' }, // Redirección a 'inicio'
+  { path: 'dashboard', component: DashboardComponent, 
+    children: [ 
     { path: 'inicio', component: InicioComponent },
     { path: 'messages', component: MessagesComponent },
-  ]},
+    { path: '', redirectTo: 'inicio', pathMatch: 'full' }
+  ]
+  },
   { path: 'admin', component: AdminComponent },
-  { path: 'user', component: UserComponent},
+  { path: 'user', component: UserComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' } 
 ];
 
